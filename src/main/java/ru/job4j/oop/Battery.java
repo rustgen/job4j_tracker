91d565e88ee7f738286a65afe9b246aeb093c0f7
rustgen <nrustgen@gmail.com>
2022-04-02 15:48:29 -0700

    1.8. Состояние объекта [#167388 #283644]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(30);
+        Battery second = new Battery(50);
+        first.getLoad();
+        second.getLoad();
+        first.exchange(second);
+        System.out.println(first.load + "----->" + second.load);
+    }
+}
