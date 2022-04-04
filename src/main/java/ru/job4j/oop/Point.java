9a093f880feb4cc928f23ecc19b9f7390db495b9
rustgen <nrustgen@gmail.com>
2022-04-04 02:40:22 -0700

    2.2. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/main/java/ru/job4j/oop/Point.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+}
