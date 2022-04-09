aa1799658ec711c13fb0aceae33a5d55b7eb3841
rustgen <nrustgen@gmail.com>
2022-04-08 22:28:55 -0700

    3.2. Удаление моделей из массива. [#310016 #286021]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -23,7 +23,6 @@ public class ShopDrop {
     }
 
     public static Product[] delete(Product[] products, int index) {
-        products[index] = null;
         for (int i = index; i < products.length - 1; i++) {
             products[i] = products[i + 1];
             }
