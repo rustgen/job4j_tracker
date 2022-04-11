2299047a98e8e46b4f02301a4322318125e797f4
rustgen <nrustgen@gmail.com>
2022-04-11 14:14:10 -0700

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,17 +3,41 @@ package ru.job4j.tracker;
 import javax.swing.text.DateFormatter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class StartUI {
 
-    public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String curentDayTime = item.getCreated().format(formatter);
-        System.out.println(curentDayTime);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
 
-        Item obj = new Item();
-        System.out.println(obj);
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+
     }
 }
