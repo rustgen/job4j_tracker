23436341f8ef69fe4a0a3cee2cad1e72843e8465
rustgen <nrustgen@gmail.com>
2022-04-11 20:41:39 -0700

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int people) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    void ride();
+
+    void passengers(int people);
+
+    int refuel(int fuel);
+}
