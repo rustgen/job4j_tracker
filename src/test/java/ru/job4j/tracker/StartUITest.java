e916f11eef8b086bc3e454046112862b545d0dc3
rustgen <nrustgen@gmail.com>
2022-04-13 22:36:40 -0700

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -15,5 +16,31 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("next item");
+        tracker.add(item);
+        int id = item.getId();
+        String[] answers = {String.valueOf(id)};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = null;
+        assertThat(StartUI.deleteItem(new StubInput(answers), tracker), is(deleted));
     }
 }
