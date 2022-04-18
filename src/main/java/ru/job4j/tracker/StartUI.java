32efc15dcd0b630a1dcb7b8dc36d4fdeb2fd8518
rustgen <nrustgen@gmail.com>
2022-04-18 01:23:54 -0700

    9.1 Зависимость от System.out [#33568 #289838]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -29,8 +29,8 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(output), new ShowAction(), new EditAction(output), new DeleteAction(),
-                new FindByIdAction(), new FindByNameAction(), new ExitAction(output)
+                new CreateAction(output), new ShowAction(output), new EditAction(output), new DeleteAction(output),
+                new FindByIdAction(output), new FindByNameAction(output), new ExitAction(output)
         };
         new StartUI(output).init(input, tracker, actions);
 
