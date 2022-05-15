6192679ce04411a70a70c4fe40d06721316ca6cb
rustgen <nrustgen@gmail.com>
2022-05-15 18:04:46 -0400

    0. Сортировка [#10096 #300791]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -3,47 +3,49 @@ package ru.job4j.tracker;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.collection.ItemAscByName;
+import ru.job4j.collection.ItemDescByName;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ItemTest {
 
     @Test
     public void compareToAsc() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item(1, "First"));
-        items.add(new Item(2, "Second"));
-        items.add(new Item(3, "Third"));
-        Collections.sort(items);
+        List<Item> items = Arrays.asList(
+                new Item("First"),
+                new Item("Second"),
+                new Item("Third")
+        );
+        Collections.sort(items, new ItemAscByName());
 
-        List<Item> expected = new ArrayList<>();
-        items.add(new Item(1, "First"));
-        items.add(new Item(2, "Second"));
-        items.add(new Item(3, "Third"));
-        Assert.assertEquals(expected, equals(Collections.sort(items)));
+        List<Item> expected = Arrays.asList(
+                new Item("First"),
+                new Item("Second"),
+                new Item("Third")
+        );
+        Assert.assertThat(expected, is(items));
     }
 
     @Test
-    public void compareToAsc() {
+    public void compareToDesc() {
         List<Item> items = Arrays.asList(
-                new Item( "First"),
-                new Item( "Second"),
+                new Item("First"),
+                new Item("Second"),
                 new Item("Third")
         );
-//        items.add(new Item( "First"));
-//        items.add(new Item( "Second"));
-//        items.add(new Item("Third"));
-        Collections.sort(items);
+        Collections.sort(items, new ItemDescByName());
 
-        List<Item> expected = new ArrayList<>();
-        items.add(new Item(1, "First"));
-        items.add(new Item(2, "Second"));
-        items.add(new Item(3, "Third"));
-        Assert.assertEquals(expected, equals(items));
+        List<Item> expected = Arrays.asList(
+                new Item("Third"),
+                new Item("Second"),
+                new Item("First")
+        );
+        Assert.assertThat(expected, is(items));
     }
 }
