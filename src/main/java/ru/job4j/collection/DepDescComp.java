cb14793d76fbb00885d75cbda2fd8af50207ce84
rustgen <nrustgen@gmail.com>
2022-05-28 23:25:13 -0700

    2. Отсортировать департаменты [#34352 #306167]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] first = o1.split("\\.");
-        String[] second = o2.split("\\.");
+        String[] first = o1.split("/");
+        String[] second = o2.split("/");
         int rsl = second[0].compareTo(first[0]);
         return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
