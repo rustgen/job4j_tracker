52f2f0e363b3b38ef82ce4114f224e1421a24fdb
rustgen <nrustgen@gmail.com>
2022-05-28 22:10:34 -0700

    2. Отсортировать департаменты [#34352 #306167]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,12 +9,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.isEmpty()) {
-                    start = el;
-                    tmp.add(start);
-                } else {
-                    tmp.add(start + "/" + el);
-                }
+                start += "".equals(start) ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
