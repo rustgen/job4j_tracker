46189f6996b9595128c37817d949e15798d9aad1
rustgen <nrustgen@gmail.com>
2022-06-04 20:34:32 -0700

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        List<Student> res = students.stream().filter(student -> predict.test(student)).collect(Collectors.toList());
+        return res;
+    }
+}
