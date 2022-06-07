8ff1bea3cfd21489e6c26d50c8d1ad17c2925a28
rustgen <nrustgen@gmail.com>
2022-06-07 01:43:43 -0700

    1. Метод .of [#70623 #309822]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -28,7 +28,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAscWithoutMissedDepartments() {
-        List<String> input = List.of(
+        List<String> input = Arrays.asList(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -56,7 +56,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAscWithMissedDepartments() {
-        List<String> input = List.of(
+        List<String> input = Arrays.asList(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1",
@@ -78,7 +78,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortDescWithoutMissedDepartments() {
-        List<String> input = List.of(
+        List<String> input = Arrays.asList(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -106,7 +106,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortDescWithMissedDepartments() {
-        List<String> input = List.of(
+        List<String> input = Arrays.asList(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
