00b4bd0780aa408a86cf11eac7cafb54646fd8f8
new_user <nrustgen@gmail.com>
2022-10-11 21:26:54 -0700

    1. Hibernate configuration [#6872 #350896]

--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -1,11 +1,11 @@
 package ru.job4j.tracker;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
