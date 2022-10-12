c9e9317b1786897467f4e657cb7121fea35f6162
new_user <nrustgen@gmail.com>
2022-10-12 01:59:55 -0700

    1. Liquibase. Install. [#504892] added profiles, update plugin, replaced old tests, delete db files and updated liquibase

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -1,20 +1,19 @@
 package ru.job4j.tracker;
 
-import org.junit.*;
-import ru.job4j.tracker.Item;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class SqlTrackerTest {
 
@@ -57,49 +56,4 @@ public class SqlTrackerTest {
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
-    @Test
-    public void whenSaveTwoItemsWithSameNameAndFindByGeneratedNameThenMustBeTwoItemsWithSameName() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item1"));
-        Item item2 = tracker.add(new Item("item2"));
-        Item item3 = tracker.add(new Item("item1"));
-        Item item4 = tracker.add(new Item("item4"));
-        String name = item1.getName();
-        List<Item> items = tracker.findByName(name);
-        assertThat(items.size(), is(2));
-    }
-
-    @Test
-    public void whenSaveFourItemsAndFindAllItemsShouldShowAllAddedItems() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item1"));
-        Item item2 = tracker.add(new Item("item2"));
-        Item item3 = tracker.add(new Item("item3"));
-        Item item4 = tracker.add(new Item("item4"));
-        assertThat(tracker.findAll(), is(List.of(item1, item2, item3, item4)));
-    }
-
-    @Test
-    public void whenSaveFourItemsAndDeleteItemsShouldShowAllLeftItems() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item1"));
-        Item item2 = tracker.add(new Item("item2"));
-        Item item3 = tracker.add(new Item("item3"));
-        Item item4 = tracker.add(new Item("item4"));
-        int id1 = item1.getId();
-        int id2 = item2.getId();
-        tracker.delete(id2);
-        assertThat(tracker.findById(id1), is(item1));
-        assertThat(tracker.findAll(), is(List.of(item1, item3, item4)));
-    }
-
-    @Test
-    public void whenSaveFourItemsAndReplaceItemsShouldShowAllItemsWithChanges() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item1"));
-        Item item5 = new Item("item5");
-        int id = item1.getId();
-        assertThat(tracker.replace(id, item5), is(true));
-        assertThat(tracker.findById(id).getName(), is("item5"));
-    }
 }
