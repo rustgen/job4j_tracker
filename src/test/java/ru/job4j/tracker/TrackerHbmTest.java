b862c512da1cbfc598c531725cee94ad72331454
new_user <nrustgen@gmail.com>
2022-10-22 18:50:44 -0700

    1. Integration testing with Hibernate [#6875]

---
+++ src/test/java/ru/job4j/tracker/TrackerHbmTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TrackerHbmTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName(), is(item.getName()));
+        }
+    }
+}
